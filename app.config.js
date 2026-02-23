04203196f99955f0229829c3d5c921a0b3d6235e
Mateusz Francik <mateusz.francik@swmansion.com>
2026-02-23 15:25:06 +0100

    (feat) Working camera, currently without frame processor

--- app.config.js
+++ app.config.js
@@ -1,54 +1,68 @@
-import 'dotenv/config';
+import "dotenv/config";
 
 export default {
-  "expo": {
-    "name": "poke-app",
-    "slug": "poke-app",
-    "version": "1.0.0",
-    "orientation": "portrait",
-    "icon": "./assets/icon.png",
-    "userInterfaceStyle": "light",
-    "newArchEnabled": true,
-    "splash": {
-      "image": "./assets/splash-icon.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
+  expo: {
+    name: "poke-app",
+    slug: "poke-app",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    newArchEnabled: true,
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
     },
-    "ios": {
-      "supportsTablet": true,
-      "bundleIdentifier": "swm.mfrancik.poke-app"
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "swm.mfrancik.poke-app",
     },
-    "android": {
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/adaptive-icon.png",
-        "backgroundColor": "#ffffff"
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff",
       },
-      "config": {
-        "googleMaps": {
-          "apiKey": "process.env.EXPO_PUBLIC_MAP_API_KEY"
-        }
+      config: {
+        googleMaps: {
+          apiKey: "process.env.EXPO_PUBLIC_MAP_API_KEY",
+        },
       },
-      "edgeToEdgeEnabled": true,
-      "predictiveBackGestureEnabled": false,
-      "permissions": [
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
+      permissions: [
         "android.permission.ACCESS_COARSE_LOCATION",
         "android.permission.ACCESS_FINE_LOCATION",
         "android.permission.ACCESS_COARSE_LOCATION",
-        "android.permission.ACCESS_FINE_LOCATION"
+        "android.permission.ACCESS_FINE_LOCATION",
       ],
-      "package": "swm.mfrancik.pokeapp"
+      package: "swm.mfrancik.pokeapp",
     },
-    "web": {
-      "favicon": "./assets/favicon.png"
+    web: {
+      favicon: "./assets/favicon.png",
     },
-    "plugins": [
+    plugins: [
       [
         "expo-maps",
         {
-          "requestLocationPermission": true,
-          "locationPermission": "Allow $(PRODUCT_NAME) to use your location"
-        }
-      ]
-    ]
-  }
-}
+          requestLocationPermission: true,
+          locationPermission: "Allow $(PRODUCT_NAME) to use your location",
+        },
+      ],
+      [
+        "react-native-vision-camera",
+        {
+          cameraPermissionText: "Allow $(PRODUCT_NAME) to use your Camera.",
+        },
+      ],
+      [
+        "expo-build-properties",
+        {
+          ios: {
+            deploymentTarget: "16.0",
+          },
+        },
+      ],
+    ],
+  },
+};
--- src/screens/CameraScreen.tsx
+++ src/screens/CameraScreen.tsx
@@ -1,9 +1,86 @@
-import { Text, View } from "react-native";
+import { useIsFocused } from "@react-navigation/native";
+import { useEffect, useRef } from "react";
+import { AppState, StyleSheet, Text, View } from "react-native";
+import {
+  Frame,
+  useCameraDevice,
+  useCameraPermission,
+  Camera,
+  runAsync,
+  useFrameProcessor,
+} from "react-native-vision-camera";
+import {
+  Face,
+  FrameFaceDetectionOptions,
+  useFaceDetector,
+} from "react-native-vision-camera-face-detector";
+import { Worklets } from "react-native-worklets-core";
 
 function CameraScreen() {
+  const { hasPermission, requestPermission } = useCameraPermission();
+  const device = useCameraDevice("front");
+  const isFocused = useIsFocused();
+  const appState = useRef(AppState.currentState);
+  const isActive = isFocused && appState.current === "active";
+
+  const faceDetectionOptions = useRef<FrameFaceDetectionOptions>({
+    // detection options
+  }).current;
+
+  const { detectFaces, stopListeners } = useFaceDetector(faceDetectionOptions);
+
+  useEffect(() => {
+    return () => {
+      // you must call `stopListeners` when current component is unmounted
+      stopListeners();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!device) {
+      // you must call `stopListeners` when `Camera` component is unmounted
+      stopListeners();
+      return;
+    }
+
+    (async () => {
+      const status = await Camera.requestCameraPermission();
+      console.log({ status });
+    })();
+  }, [device]);
+
+  const handleDetectedFaces = Worklets.createRunOnJS((faces: Face[]) => {
+    console.log("faces detected", faces);
+  });
+
+  const frameProcessor = useFrameProcessor(
+    (frame) => {
+      "worklet";
+      runAsync(frame, () => {
+        "worklet";
+        const faces = detectFaces(frame);
+        // ... chain some asynchronous frame processor
+        // ... do something asynchronously with frame
+        handleDetectedFaces(faces);
+      });
+      // ... chain frame processors
+      // ... do something with frame
+    },
+    [handleDetectedFaces],
+  );
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Camera Screen</Text>
+    <View style={{ flex: 1 }}>
+      {!!device ? (
+        <Camera
+          style={StyleSheet.absoluteFill}
+          device={device}
+          isActive={true}
+          frameProcessor={frameProcessor}
+        />
+      ) : (
+        <Text>No Device</Text>
+      )}
     </View>
   );
 }
